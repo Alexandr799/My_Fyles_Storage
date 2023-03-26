30d9992390dd30fe445aef1c6ab885c5af3a79dc
Alex Strigo <huligangster1488@gmail.com>
2023-03-26 21:26:13 +0300

    initial-commit

---
+++ build/precache-manifest.24d7a36a7829c688e76c1deae31a0ea7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "145a1a91748f0d662e2b",
+    "url": "./static/css/main.b8eda791.chunk.css"
+  },
+  {
+    "revision": "145a1a91748f0d662e2b",
+    "url": "./static/js/main.145a1a91.chunk.js"
+  },
+  {
+    "revision": "b8d60a2da83de4a28b21",
+    "url": "./static/css/1.6eabaa25.chunk.css"
+  },
+  {
+    "revision": "b8d60a2da83de4a28b21",
+    "url": "./static/js/1.b8d60a2d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "287aa9f903b43e9342b0e83d2ea1030c",
+    "url": "./static/media/logo.287aa9f9.svg"
+  },
+  {
+    "revision": "15ae26ba3634c3734f3c8cd9c19bd801",
+    "url": "./static/media/exit.15ae26ba.svg"
+  },
+  {
+    "revision": "cca7d5f3b958879f7af79ed67a3fb838",
+    "url": "./index.html"
+  }
+];
