2a0c40e8feaa00beb2e607ac1bbf4744eb9282d3
Alex Strigo <huligangster1488@gmail.com>
2023-04-22 18:00:06 +0300

    add-readme

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-
-$a = password_hash(123, PASSWORD_DEFAULT);
-$b = password_hash(123, PASSWORD_DEFAULT);
-
-var_dump(password_verify('qwerty', '$2y$10$u7USdpFRSMaP6i/d9DN4GuBUAIPoV3tIJljYeUMVdB.e8H5vVcFny'));
