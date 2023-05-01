616298141467ef06266829cd73bd6ddd4439d420
Alex Strigo <huligangster1488@gmail.com>
2023-05-01 17:15:44 +0300

    init-delete-recursive-dir

--- src/app/Controllers/File.php
+++ src/app/Controllers/File.php
@@ -194,57 +194,17 @@ class File extends Controller
 
     public function deleteDirectory(Request $req)
     {
-        $files = [];
-        $directories = [];
-
         $db = DataBase::create();
-
         $dirInfo = $db->quary(
-            "SELECT id, pwd FROM `directories`  WHERE id = :id AND owner_user_id=:owner",
+            "SELECT id, pwd, parent_dir_id FROM `directories`  WHERE id = :id AND owner_user_id=:owner",
             ['id' => $req->getArg('id'), 'owner' => Response::getSession('id')]
         );
 
         if (!$dirInfo['success']) Response::json(['error' => 'Что то пошло не так...'], 500);
         if (count($dirInfo['data']) === 0) Response::json(['error' => 'Такой директории нет, или вы не имеете к нему доступа'], 404);
+        $dirInfo  = $dirInfo['data'];
 
-        $dirInfo = $dirInfo['data'][0];
-        if ($dirInfo['pwd'] === '/') Response::json(['error' => 'Корневую директорию можно удалить, только в случае удаления аккаунта!'], 400);
-
-        $db->startTransaction();
-        try {
-
-            $filesOld =  $db->quaryTransaction(
-                "SELECT GROUP_CONCAT(id SEPARATOR '===') as id 
-                FROM files WHERE owner_user_id=:id",
-                ['id' => Response::getSession('id')]
-            );
-            $filesOld  = explode('===', $filesOld[0]['id']);
-            unset($filesOld[0]);
-            $filesOld = array_values($filesOld);
-            var_dump($filesOld);
-
-
-            // $db->quaryTransaction(
-            //     'DELETE FROM `directories` WHERE id=:id',
-            //     ['id' => Response::getSession('id')]
-            // );
-
-            $filesNew =  $db->quaryTransaction(
-                "SELECT GROUP_CONCAT(id SEPARATOR '===') as id
-                FROM files WHERE owner_user_id=:id",
-                ['id' => Response::getSession('id')]
-            );
-
-            $filesNew  = explode('===', $filesNew[0]['id']);
-            var_dump($filesNew);
-            var_dump(array_diff($filesNew, $filesOld));
-
-
-            
-        } catch (Exception $e) {
-            $db->cancelTransaction();
-            Logger::printLog($e->getMessage(), 'db');
-            Response::json(['error' => 'Не удалить директорию'], 500);
-        }
+        $files = FileStorage::getAllFileRecursive($dirInfo['id'], $dirInfo['parent_dir_id']);
+        
     }
 }
--- src/app/Custom/FileStorage.php
+++ src/app/Custom/FileStorage.php
@@ -6,7 +6,7 @@ use Exception;
 
 class FileStorage
 {
-    public static function renameFile(string $fileName, int | string $id,  string $newname)
+    public static function renameFile(string $fileName, int | string $id,  string $newname): void
     {
         $name = realpath('./storage/filestorage') . "/$id" . '_' . $fileName;
         $new = realpath('./storage/filestorage') . "/$id" . '_' . $newname;
@@ -14,15 +14,38 @@ class FileStorage
         if (!$result) throw new Exception("Не удалось переименовать файл!");
     }
 
-    public static function addFile(array $file, int | string $idFile)
+    public static function addFile(array $file, int | string $idFile): void
     {
         $result = move_uploaded_file($file["tmp_name"], realpath('./storage/filestorage') . "/$idFile" . '_' . $file['name']);
         if (!$result) throw new Exception("Не удалось создать файл!");
     }
 
-    public static function deleteFile(string $file, int | string $idFile)
+    public static function deleteFile(string $file, int | string $idFile): void
     {
         $result = unlink(realpath('./storage/filestorage') . "/$idFile" . '_' . $file);
         if (!$result) throw new Exception("Не удалось удалить файл!");
+    }
+
+    public static function deleteFileByName(string $filename): void
+    {
+        $result = unlink(realpath('./storage/filestorage') . "/$filename");
+        if (!$result) throw new Exception("Не удалось удалить файл!");
+    }
+
+
+    private static function concatStringElement(string $first, string $second, $sep): array
+    {
+        $firstExp  = explode($sep, $first);
+        $secondExp  = explode($sep, $second);
+        return array_map(function ($id, $name) {
+            return $id . '_' . $name;
+        }, $firstExp,  $secondExp);
+    }
+
+    public static function getAllFileRecursive(int | string $id, null | string | int $parent_dir_id, &$files = []):array
+    {
+        if (empty($parent_dir_id)) {
+            
+        }
     }
 }
